1a5e899d65b49b8e48635688c79809526a9d4255
Priyasharma04 <ayirp.0404@gmail.com>
2025-11-19 07:58:49 +0530

    ngrok changed

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-NGROK_URL = "https://shrubby-glenda-sacramentally.ngrok-free.dev"  
+NGROK_URL = "https://inventional-yvette-drily.ngrok-free.dev"  
 @app.post("/api/review")
 async def review(file: UploadFile = File(...)):
     try:
